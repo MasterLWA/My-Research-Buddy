10522c14291f28acf13de80a3eccefa1e18a8b98
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-06-26 00:04:10 +0530

    Connected to the database

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,33 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
-const port = 4000;
+// const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Backend working well!');
-});
+// Load environment variables from .env file
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+
+//middleware
+app.use(express.json()); 
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+// Connect to the database and start the server
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // returns a promise
+  
+  .then(() => { // if the promise is resolved
+    console.log('Connected to the database');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  })
+  
+  .catch((error) => {
+    console.log('Error connecting to the database:', error);
+  });
