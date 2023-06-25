304b7266a9a12fd5887e8d0ad5f82f227df6f6e1
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-06-26 01:32:33 +0530

    Update User

--- server/src/Router/UsersRoute.js
+++ server/src/Router/UsersRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createUser, getUserById, getAllUsers} = require('../Controller/UsersController.js');
+const {createUser, getUserById, getAllUsers,updateUser} = require('../Controller/UsersController.js');
 
 //get all users
 router.get('/users', getAllUsers);
@@ -12,6 +12,7 @@ router.get('/users/:id', getUserById);
 router.post('/users', createUser);
 
 //update a user
+router.patch('/users/:id', updateUser);
 
 //delete a user
 
