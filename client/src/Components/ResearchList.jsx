700bccb71c0d84886bbb9fd69df8681bc7e32eaf
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-08-13 15:27:35 +0530

    List of Reseach component

---
+++ client/src/Components/ResearchList.jsx
@@ -0,0 +1,43 @@
+//creat new function
+//import react
+
+
+const ResearchList = () => {
+    return(
+        <div>
+            {/* Demo data 1 */}
+            <div class="card">
+                <h5 class="card-header text-white bg-primary">Reseacrh 2</h5>
+                    <div class="card-body">
+                        <h5 class="card-title" id="research-topic">Special title treatment</h5>
+                        <p class="card-text" id="research-discription">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+
+            {/* Demo data 2 */}
+            <div class="card">
+                <h5 class="card-header text-white bg-primary">Reseacrh 2</h5>
+                    <div class="card-body">
+                        <h5 class="card-title" id="research-topic">Special title treatment</h5>
+                        <p class="card-text" id="research-discription">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+
+            {/* Demo data 3 */}
+            <div class="card">
+                <h5 class="card-header text-white bg-primary">Reseacrh 2</h5>
+                    <div class="card-body">
+                        <h5 class="card-title" id="research-topic">Special title treatment</h5>
+                        <p class="card-text" id="research-discription">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default ResearchList;
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,15 @@
 import * as Icon from 'react-bootstrap-icons';
+import ResearchList from '../../Components/ResearchList';
 const Dashboard = () => {
     return (
         <div className={"container-fluid"}>
 
-            <div className={"container border border-dark rounded content-justify-center"} style={{marginTop: "5%"}}>
+            <div className={"container content-justify-center"} style={{marginTop: "5%"}}>
                 {/*row*/}
                 <div className={"row"}> 
                     {/* list of on going researches */}
-                    <div className={"col-md-8 border border-dark rounded content-justify-center"}>
-                            <p>This is list of reseaches</p>
+                    <div className={"col-md-8"}>
+                            <ResearchList />
                     </div>
 
                     {/* Buttons to navigate other functions */}
